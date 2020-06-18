db0e0f4ab1699202525737a052e82e0b7007172f
yudai <yudaikawabuchi@gmai.com>
2020-06-18 20:15:01 +0900

    ログインログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_page#home'
+  get 'users/new'
   get '/signup', to:'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
